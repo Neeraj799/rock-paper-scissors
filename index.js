7c112599d797382951b220d4924d53b70ed913c7
neeraj <neerajpc799@gmail.com>
2023-02-15 22:00:03 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-function ahhd()
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection)
+    return "It's a tie";
+
+    else if(
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock" ) ||
+        (playerSelection === "scissors" && computerSelection ==="paper")
+        ) {
+            return "You Win!"
+        } else {
+            return "You Lose!"
+        }
+}
+
+    
